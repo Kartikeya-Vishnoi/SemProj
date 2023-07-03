1537af2716e1177a73be78f259e4a1321118bdd8
AryanJswl <112781350+AryanJswl@users.noreply.github.com>
2023-07-04 03:39:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ then
      docker stop bridge
      docker rm bridge
 fi
-docker run -d --name=bridge -p 3000:3000 -p 8900:8900 -p 8080:8080 kubearyan/bridge:latest
+docker run -d --name=bridge -p 3000:3000 -p 8900:8900 -p 8080:8080 kubearyan/bridge_app:latest
